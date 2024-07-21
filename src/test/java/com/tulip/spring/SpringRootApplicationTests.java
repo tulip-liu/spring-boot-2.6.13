3058aefe9b5f7c48aaa8b4c4744dcb6286b6859c
liuhaifu <tulip8991@foxmail.com>
2024-07-21 21:04:50 +0800

    spring-boot version 2.6.13 init project

---
+++ src/test/java/com/tulip/spring/SpringRootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tulip.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
